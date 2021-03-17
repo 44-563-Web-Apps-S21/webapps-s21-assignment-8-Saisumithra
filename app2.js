4669a4a0563308d94afdcd5eec2c1834111a922e
Saisumithra <77755086+Saisumithra@users.noreply.github.com>
2021-03-17 14:51:21 -0500

    changes made to app2.js

--- app2.js
+++ app2.js
@@ -13,7 +13,13 @@ const server =
       res.write('<body>')
       res.write('Content \n')
       res.write('More content \n')
-      res.write('Hello World')    
+      res.write('<table>')
+      res.write('<tr><th>Location</th><th>Number of Hours</th><th>Money</th></tr>')    
+      res.write('<tr><td>California</td><td>8 Hours</td><td>$40</td></tr>')
+      res.write('<tr><td>Sun Valley</td><td>12 Hours</td><td>$200</td><tr>')
+      res.write('<tr><td>Colorado</td><td>15 Hours</td><td>$300</td></tr>')
+      res.write('<tr><td>New Mexico</td><td>16 Hours</td><td>$400</td></tr>')
+      res.write('</table>')
       res.end('</body></html>')
       //Close the response
     }                           
